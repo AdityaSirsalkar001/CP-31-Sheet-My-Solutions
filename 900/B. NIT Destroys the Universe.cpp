cfa38d49baad7ba4fd06ae35dd801f053bc314b6
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2025-06-18 23:53:50 +0530

    Create B. NIT Destroys the Universe.cpp

---
+++ 900/B. NIT Destroys the Universe.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <cstring>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define pii pair<int, int>
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
+const int MOD = 1e9 + 7;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define in(a, n) rep(i, 0, n) cin >> a[i]
+#define out(a) for (auto x : a) cout << x << " "; cout << '\n'
+void solve() {
+    // your solution code
+    int n;
+    cin >> n;
+    vi arr(n);
+    
+    in(arr , n);
+    
+    int cnt = 0;
+    rep(i , 0 , n-1){
+        if(arr[i] != 0 && arr[i+1] == 0){
+            cnt++;
+           
+        }
+       
+        
+    }
+    if(arr[n-1] != 0)cnt++;
+    
+    cnt = min(cnt , 2LL);
+
+     cout << cnt << endl;
+}
+int32_t main() {
+    fastio();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
