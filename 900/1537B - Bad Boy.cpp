baf4c03268fe0dd867336e329854077802169257
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2025-07-24 22:41:36 +0530

    Create 1537B - Bad Boy.cpp

---
+++ 900/1537B - Bad Boy.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n , m , a , b;
+        cin >> n >> m >> a >> b;
+        cout << n << " " << m << " " << "1 1" << endl; 
+    }
+}
