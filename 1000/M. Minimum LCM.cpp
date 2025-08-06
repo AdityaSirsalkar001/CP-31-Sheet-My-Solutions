b1fcba9efda97bd60767b14cebddc26c7eedf7e9
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2025-08-06 17:31:07 +0530

    Added Solution to Minimum LCM

---
+++ 1000/M. Minimum LCM.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <cstring>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define pii pair<int, int>
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
+const int MOD = 1e9 + 7;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define in(a, n) rep(i, 0, n) cin >> a[i]
+#define out(a) for (auto x : a) cout << x << " "; cout << '\n'
+void solve() {
+    // your solution code
+
+    int n;
+    cin >> n;
+
+    if(n % 2 == 0){
+        cout << n/2 << " " << n/2 << endl;
+    }
+
+    else {
+        int mini = n;
+        
+        for(int i = 2; i*i <= n; i++){
+            if(n % i == 0){
+                mini = i;
+                break;
+              
+            }
+        }
+
+        cout <<  (n/mini) << " " << (mini - 1) *(n/mini) << endl;
+    }
+
+}
+int32_t main() {
+    fastio();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
