b9f6155fec7b889d52524d9b8da3514d6fadda14
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2025-08-22 06:55:32 +0530

    Update

--- 1200/B_Playing_in_a_Casino.cpp
+++ 1200/B_Playing_in_a_Casino.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <stack>
-#include <cmath>
-#include <climits>
-#include <cstring>
 using namespace std;
+
 #define int long long
 #define vi vector<int>
-#define pii pair<int, int>
-#define pb push_back
-#define ff first
-#define ss second
-#define all(x) (x).begin(), (x).end()
 #define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
-const int MOD = 1e9 + 7;
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
 #define rep(i, a, b) for (int i = a; i < b; ++i)
-#define rrep(i, a, b) for (int i = a; i >= b; --i)
-#define each(x, a) for (auto &x : a)
-#define in(a, n) rep(i, 0, n) cin >> a[i]
-#define out(a) for (auto x : a) cout << x << " "; cout << '\n'
 
 void solve() {
     int n , m;
     cin >> n >> m;
 
-    vector<vector<int>> a()
+    vector<vector<int>> a(n, vector<int>(m)); 
+    rep(i, 0, n) {
+        rep(j, 0, m) {
+            cin >> a[i][j];
+        }
+    }
+
+    int ans = 0;
+
+ 
+    rep(col, 0, m) {
+        vector<int> vals;
+        rep(row, 0, n) vals.push_back(a[row][col]);
+
+        sort(vals.begin(), vals.end());
+
+        rep(i, 0, n) {
+            ans += vals[i] * (2LL * i - (n - 1));
+        }
+    }
+
+    cout << ans << "\n";
 }
+
 int32_t main() {
     fastio();
     int t = 1;
