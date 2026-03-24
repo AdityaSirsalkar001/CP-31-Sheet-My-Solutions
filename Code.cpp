c0c5cd67ff6e5810d50080e989658540bd633013
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2026-03-24 14:48:59 +0530

    Refactor macros and improve function definitions

--- Code.cpp
+++ Code.cpp
@@ -10,112 +10,95 @@
 #include <cmath>
 #include <climits>
 #include <cstring>
+
 using namespace std;
+
 #define int long long
 #define vi vector<int>
-#define pii pair<int, int>
+#define pii pair<int,int>
 #define pb push_back
 #define ff first
 #define ss second
 #define all(x) (x).begin(), (x).end()
+
 #define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
+
 const int MOD = 1e9 + 7;
+
 #define yes cout << "YES\n"
 #define no cout << "NO\n"
-#define rep(i, a, b) for (int i = a; i < b; ++i)
-#define rrep(i, a, b) for (int i = a; i >= b; --i)
-#define each(x, a) for (auto &x : a)
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define each(x,a) for(auto &x:a)
+
 #define in(a, n) rep(i, 0, n) cin >> a[i]
-#define out(a) for (auto x : a) cout << x << " "; cout << '\n'
+#define out(a)            \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << '\n'
+    
+int gcd(int a,int b){ return b==0?a:gcd(b,a%b); }
+int lcm(int a,int b){ return (a/gcd(a,b))*b; }
 
-void solve() {
-    int n;
-    cin >> n;
-
-    int ans = 0;
-    int div = 3;
-    int x = 0;
-    int val = 3;
-    int temp = 0;
-    while(n){
-        while(n%div == 0 && div < n){
-            div *= 3;
-        }
-        temp = (n%div)*val;#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <stack>
-#include <cmath>
-#include <climits>
-#include <cstring>
-using namespace std;
-#define int long long
-#define vi vector<int>
-#define pii pair<int, int>
-#define pb push_back
-#define ff first
-#define ss second
-#define all(x) (x).begin(), (x).end()
-#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
-const int MOD = 1e9 + 7;
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-#define rep(i, a, b) for (int i = a; i < b; ++i)
-#define rrep(i, a, b) for (int i = a; i >= b; --i)
-#define each(x, a) for (auto &x : a)
-#define in(a, n) rep(i, 0, n) cin >> a[i]
-#define out(a) for (auto x : a) cout << x << " "; cout << '\n'
-
-void solve() {
-    int n;
-    cin >> n;
-
-    int ans = 0;
-    int div = 3;
-    int x = 0;
-    int val = 3;
-    int temp = 0;
-    while(n){
-        while(n%div == 0 && div < n){
-            div *= 3;
-        }
-        temp = (n%div)*val;
-        div *= 3;
-        x++;
-        val = pow(3 , x+1) + (x*pow(3 , x-1));
-        n -= temp;
-        ans += temp;
+int binpow(int a,int b){
+    int res=1;
+    while(b){
+        if(b&1) res=(res*a)%MOD;
+        a=(a*a)%MOD;
+        b>>=1;
     }
-    cout << ans << endl;
+    return res;
 }
-int32_t main() {
+
+void dfs(int node, vector<vector<int>> &adj, vector<int> &vis){
+    vis[node]=1;
+    for(auto nxt: adj[node]){
+        if(!vis[nxt]) dfs(nxt, adj, vis);
+    }
+}
+
+void bfs(int start, vector<vector<int>> &adj, vector<int> &vis){
+    queue<int> q;
+    q.push(start);
+    vis[start]=1;
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+
+        for(auto nxt: adj[node]){
+            if(!vis[nxt]){
+                vis[nxt]=1;
+                q.push(nxt);
+            }
+        }
+    }
+}
+
+void solve(){
+    
+}
+
+int32_t main(){
     fastio();
-    int t = 1;
-    cin >> t;
-    while (t--) {
+
+    int t=1;
+    cin>>t;
+    while(t--){
         solve();
     }
     return 0;
 }
-        div *= 3;
-        x++;
-        val = pow(3 , x+1) + (x*pow(3 , x-1));
-        n -= temp;
-        ans += temp;
-    }
-    cout << ans << endl;
-}
-int32_t main() {
-    fastio();
-    int t = 1;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
