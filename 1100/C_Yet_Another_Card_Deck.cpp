a2146faa7f55d33bb79becdfe6351cd58a2d69a7
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2026-03-25 12:32:19 +0530

    Add files via upload

---
+++ 1100/C_Yet_Another_Card_Deck.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+const int MOD = 1e9 + 7;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define each(x,a) for(auto &x:a)
+
+#define in(a, n) rep(i, 0, n) cin >> a[i]
+#define out(a)            \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << '\n'
+    
+int gcd(int a,int b){ return b==0?a:gcd(b,a%b); }
+int lcm(int a,int b){ return (a/gcd(a,b))*b; }
+
+int binpow(int a,int b){
+    int res=1;
+    while(b){
+        if(b&1) res=(res*a)%MOD;
+        a=(a*a)%MOD;
+        b>>=1;
+    }
+    return res;
+}
+
+void dfs(int node, vector<vector<int>> &adj, vector<int> &vis){
+    vis[node]=1;
+    for(auto nxt: adj[node]){
+        if(!vis[nxt]) dfs(nxt, adj, vis);
+    }
+}
+
+void bfs(int start, vector<vector<int>> &adj, vector<int> &vis){
+    queue<int> q;
+    q.push(start);
+    vis[start]=1;
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+
+        for(auto nxt: adj[node]){
+            if(!vis[nxt]){
+                vis[nxt]=1;
+                q.push(nxt);
+            }
+        }
+    }
+}
+
+void solve(){
+    int n , k;
+    cin >> n >> k;
+    vi a(n) , b(k);
+    in(a , n);
+    in(b , k);
+    
+    vi top(51 , -1);
+    rep(i , 0 , n){
+        if(top[a[i]] == -1){
+            top[a[i]] = i;
+        } 
+    }
+
+    rep(i , 0 , k){
+        cout << top[b[i]] + 1 << " ";
+        rep(j , 0 , 51){
+            if(top[j] < top[b[i]] && top[j] >= 0) top[j]++;
+        }
+        top[b[i]] = 0;
+    }
+
+}
+
+int32_t main(){
+    solve();
+}
+
+
+
+
+
+
+
+
+
+
+
+
