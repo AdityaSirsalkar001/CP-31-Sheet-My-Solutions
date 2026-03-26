f83f51a84f42e531cb1030628f2974cd6e26a599
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2026-03-26 17:54:23 +0530

    Add files via upload

---
+++ 1100/B_GCD_Partition.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+const int MOD = 1e9 + 7;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define each(x,a) for(auto &x:a)
+
+#define in(a, n) rep(i, 0, n) cin >> a[i]
+#define out(a)            \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << '\n'
+    
+int gcd(int a,int b){ return b==0?a:gcd(b,a%b); }
+int lcm(int a,int b){ return (a/gcd(a,b))*b; }
+
+int binpow(int a,int b){
+    int res=1;
+    while(b){
+        if(b&1) res=(res*a)%MOD;
+        a=(a*a)%MOD;
+        b>>=1;
+    }
+    return res;
+}
+
+void dfs(int node, vector<vector<int>> &adj, vector<int> &vis){
+    vis[node]=1;
+    for(auto nxt: adj[node]){
+        if(!vis[nxt]) dfs(nxt, adj, vis);
+    }
+}
+
+void bfs(int start, vector<vector<int>> &adj, vector<int> &vis){
+    queue<int> q;
+    q.push(start);
+    vis[start]=1;
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+
+        for(auto nxt: adj[node]){
+            if(!vis[nxt]){
+                vis[nxt]=1;
+                q.push(nxt);
+            }
+        }
+    }
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    vi a(n);
+    in(a , n);
+    vi prefix(n+1 , 0);
+    rep(i , 1 , n+1){
+        prefix[i] = a[i-1] + prefix[i-1];
+    }
+
+    int ans = 0;
+    rep(i , 1 , n){
+        ans = max(ans , gcd(prefix[i] , prefix[n] - prefix[i]));
+    }
+    cout << ans << endl;
+}
+
+int32_t main(){
+    fastio();
+
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
