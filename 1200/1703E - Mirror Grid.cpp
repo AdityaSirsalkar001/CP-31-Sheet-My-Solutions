bf72a0fc9f185967d5eeadc5f478fe8f35883193
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2026-04-07 09:23:51 +0530

    Create  1703E - Mirror Grid.cpp

---
+++ 1200/1703E - Mirror Grid.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+const int MOD = 1e9 + 7;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define each(x,a) for(auto &x:a)
+
+#define in(a, n) rep(i, 0, n) cin >> a[i]
+#define out(a)            \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << '\n'
+    
+int gcd(int a,int b){ return b==0?a:gcd(b,a%b); }
+int lcm(int a,int b){ return (a/gcd(a,b))*b; }
+
+int binpow(int a,int b){
+    int res=1;
+    while(b){
+        if(b&1) res=(res*a)%MOD;
+        a=(a*a)%MOD;
+        b>>=1;
+    }
+    return res;
+}
+
+void dfs(int node, vector<vector<int>> &adj, vector<int> &vis){
+    vis[node]=1;
+    for(auto nxt: adj[node]){
+        if(!vis[nxt]) dfs(nxt, adj, vis);
+    }
+}
+
+void bfs(int start, vector<vector<int>> &adj, vector<int> &vis){
+    queue<int> q;
+    q.push(start);
+    vis[start]=1;
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+
+        for(auto nxt: adj[node]){
+            if(!vis[nxt]){
+                vis[nxt]=1;
+                q.push(nxt);
+            }
+        }
+    }
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<string> a(n);
+    rep(i , 0 , n){
+            cin >> a[i];
+    }
+
+    //i,j
+    //j , n-i
+    //n-i , n-j
+    //n-j , i
+   int ans = 0;
+    rep(i , 0 , (n+1)/2){
+        rep(j , 0 , (n)/2){
+            int cnt = 0;
+            if(a[i][j] == '1')cnt++;
+            if(a[j][n-1-i] == '1')cnt++;
+            if(a[n-1-i][n-1-j] == '1')cnt++;
+            if(a[n-1-j][i] == '1')cnt++;
+            if(cnt == 2) ans += 2;
+            else if(cnt == 1 || cnt == 3) ans += 1;
+            //cout << i << " " << j << " " << cnt << endl; 
+        }
+    }
+    cout << ans << endl;
+}
+
+int32_t main(){
+    fastio();
+
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
