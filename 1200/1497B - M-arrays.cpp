0c5a824ee35e4dbe95cd92528e6f5826e6e3d522
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2026-04-17 17:45:30 +0530

    Create 1497B - M-arrays.cpp

---
+++ 1200/1497B - M-arrays.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+const int MOD = 1e9 + 7;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define each(x,a) for(auto &x:a)
+
+#define in(a, n) rep(i, 0, n) cin >> a[i]
+#define out(a)            \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << '\n'
+    
+int gcd(int a,int b){ return b==0?a:gcd(b,a%b); }
+int lcm(int a,int b){ return (a/gcd(a,b))*b; }
+
+int binpow(int a,int b){
+    int res=1;
+    while(b){
+        if(b&1) res=(res*a)%MOD;
+        a=(a*a)%MOD;
+        b>>=1;
+    }
+    return res;
+}
+
+void dfs(int node, vector<vector<int>> &adj, vector<int> &vis){
+    vis[node]=1;
+    for(auto nxt: adj[node]){
+        if(!vis[nxt]) dfs(nxt, adj, vis);
+    }
+}
+
+void bfs(int start, vector<vector<int>> &adj, vector<int> &vis){
+    queue<int> q;
+    q.push(start);
+    vis[start]=1;
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+
+        for(auto nxt: adj[node]){
+            if(!vis[nxt]){
+                vis[nxt]=1;
+                q.push(nxt);
+            }
+        }
+    }
+}
+
+void solve(){
+    int n , m;
+    cin >> n >> m;
+    vi a(n);
+    in(a , n);
+
+    vector<int> mp(m , 0);
+    for(auto c : a){
+        mp[c % m]++;
+    }
+
+    //for(int i = 0; i < m; i++) cout << mp[i] << " ";
+    //cout << endl;
+    int pairs = (mp[0] > 0)? 1 : 0;
+     //cout << pairs << endl;
+    // cout << pairs << endl;
+    if(m % 2 == 0 && mp[m/2]){
+        pairs++;
+    }
+    //cout << pairs << endl;
+    for(int i = 1; i < m; i++){
+        
+        // bool take = false;
+        // while(mp[i] > 0 && mp[m-i] > 0){
+        //     mp[i]--;
+        //     mp[m-i-1]--;
+        //     take = true;
+        // }
+        
+        // if(take) pairs++;
+
+        // int rem = max(mp[i] , mp[m-i]);
+        // if(rem > 0){
+        //     pairs += rem - 1;
+        // }
+        // cout << i << " " << pairs << endl;
+
+        if(i == (m+1)/2) break;
+        
+        int mini = min(mp[i] , mp[m - i]);
+        int maxi = max(mp[i] , mp[m-i]);
+
+        if(mini > 0){
+            pairs++;
+            maxi -= mini;
+        }
+
+        if(maxi && mini != 0) pairs += maxi - 1;
+        else pairs += maxi;
+    }
+
+    cout << pairs << endl;
+}
+
+int32_t main(){
+    fastio();
+
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
