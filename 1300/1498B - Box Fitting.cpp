a5d57b572011a1a216ba24a0a95abea357843fd2
Aditya Sirsalkar <adityasirsalkar33@gmail.com>
2026-06-20 14:32:54 +0530

    Add Box Fitting problem solution in C++

---
+++ 1300/1498B - Box Fitting.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include<numeric>
+
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+const int MOD = 1e9 + 7;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define each(x,a) for(auto &x:a)
+
+#define in(a, n) rep(i, 0, n) cin >> a[i]
+#define out(a)            \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << '\n'
+    
+int gcd(int a,int b){ return b==0?a:gcd(b,a%b); }
+int lcm(int a,int b){ return (a/gcd(a,b))*b; }
+
+int binpow(int a,int b){
+    int res=1;
+    while(b){
+        if(b&1) res=(res*a)%MOD;
+        a=(a*a)%MOD;
+        b>>=1;
+    }
+    return res;
+}
+
+void dfs(int node, vector<vector<int>> &adj, vector<int> &vis){
+    vis[node]=1;
+    for(auto nxt: adj[node]){
+        if(!vis[nxt]) dfs(nxt, adj, vis);
+    }
+}
+
+void bfs(int start, vector<vector<int>> &adj, vector<int> &vis){
+    queue<int> q;
+    q.push(start);
+    vis[start]=1;
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+
+        for(auto nxt: adj[node]){
+            if(!vis[nxt]){
+                vis[nxt]=1;
+                q.push(nxt);
+            }
+        }
+    }
+}
+
+void solve(){
+    
+    int n , w;
+    cin >> n >> w;
+    vi a(n);
+    in(a , n);
+
+    sort(all(a));
+
+    int sum = accumulate(all(a) , 0);
+    int minHeight = (sum + w - 1)/w;
+
+    multiset<int> ms;
+    rep(i , 0 , n) ms.insert(a[i]);
+    int h = 1;
+    int row = w;
+    
+    while(!ms.empty()){
+        auto it = ms.upper_bound(row);
+        if(it != ms.begin()){
+            it--;
+            row -= *it;
+            ms.erase(it);
+        }
+        else row = w , h++;
+    }
+    cout << h << endl;
+    
+}
+
+int32_t main(){
+    fastio();
+
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
